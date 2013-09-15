2136d67fe4a55b569c8555d74571c4b9bccf6dab
Lucas Hoffmann <l-m-h@web.de>
2013-09-15 02:11:43 +0200

    autocomit

--- gitautocommit.sh
+++ gitautocommit.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-alias git='git --work-tree=$HOME/.config --git-dir=$HOME/.config/.git'
-
-if [ "`git status --silent --untracked-files=no`" != "" ]; then
-  git commit --all --message=autocomit
-fi
+alias git='git --work-tree="$arg" --git-dir="$arg"/.git'
+pwd
+for arg; do
+  if [ -d "$arg" ]; then
+    if [ "`git status --short --untracked-files=no`" != "" ]; then
+      git commit --all --message=autocomit
+    fi
+  fi
+done
