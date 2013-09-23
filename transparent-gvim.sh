0698b1c8a096dcb19dc03e0995eeedc2a343d35d
Lucas Hoffmann <l-m-h@web.de>
2013-09-23 11:00:03 +0200

    transparent-gvim.sh: bugfix and refactor
    Fix a bug where when no vim server is running the script fails.  Also
    refactor script to remove unused code.

--- transparent-gvim.sh
+++ transparent-gvim.sh
@@ -4,25 +4,28 @@
 # info or to call a gvim server.
 
 # find a vim server
-SERVER=`vim --serverlist | head -n 1`
-PROG=`basename "$0" | tr ' ' _`
+#PROG=`basename "$0" | tr ' ' _`
 TMP=`mktemp -t $PROG.$$.XXXXXX`
+#WAIT=false
 SAVE_STDIN=false
-if [ -z $SERVER ]; then
-  ( gvim && sleep 2 ) >/dev/null 2>&1
+if [ -z "`vim --serverlist`" ]; then
+  gvim &
 fi
-SERVER=`vim --serverlist | head -n 1`
-CMD=vimserver
+
+#CMD=vimserver
 
 # other functions
 save_stdin () { cat > $TMP; }
 
 # wrapper functions for basic interaction with Vim
-vimserver ()  { vim --servername "$SERVER" "$@"; }
-call ()       { vimserver --remote-expr "$@"; }
-foreground () { vimserver --remote-expr 'foreground()' > /dev/null; }
-send ()       { vimserver --remote-send "<C-\><C-N>$*"; }
-tab ()        { vimserver --remote-tab-wait-silent "$@"; }
+#vimserver ()  { vim --servername "$SERVER" "$@"; }
+#call ()       { vimserver --remote-expr "$@"; }
+foreground () { wait_server && vim --remote-expr 'foreground()' > /dev/null; }
+#send ()       { vimserver --remote-send "<C-\><C-N>$*"; }
+tab ()        { wait_server && vim --remote-tab-wait-silent "$@"; }
+wait_server () {
+  vim -u NONE --cmd 'while serverlist() == "" | sleep 100m | endwhile | quit'
+}
 
 # generic function to open documentation
 doc () {
@@ -31,7 +34,22 @@ doc () {
     exit 2
   fi
   call "LucTManWrapper('"$1"', '"${@:2}"')" > /dev/null
-  foreground
+  #foreground
+}
+doc () {
+  if [ $# -eq 0 ]; then
+    echo Topic needed >&2
+    exit 2
+  fi
+  vim -u NONE --cmd "
+    while serverlist() == ''
+      sleep 100m
+    endwhile
+    let s = split(serverlist())[0]
+    call remote_expr(s, 'LucTManWrapper(\"$1\", \"${@:2}\")')
+    call remote_foreground(s)
+    quit
+  "
 }
 
 # wrapper functions for individual help systems
@@ -44,20 +62,6 @@ pydoc   () { doc py  "$@"; }
 if [ $# -eq 0 ]; then
   CMD=foreground
 else
-  #while getopts fimpsty FLAG; do
-  #  case $FLAG in
-  #    i) CMD=info;;
-  #    m) CMD=man;;
-  #    p) CMD=perldoc;;
-  #    s) CMD=send;;
-  #    t) CMD='vimserver --remote-tab-wait-silent';;
-  #    y) CMD=pydoc;;
-  #    *)
-  #      echo Unkown action! Aborting ... >&2
-  #      exit 2
-  #      ;;
-  #  esac
-  #done
   # trying to implement long options
   for arg; do
     case "$arg" in
@@ -66,8 +70,8 @@ else
       --perldoc|--perl|--pl) CMD=perldoc;;
       --php) CMD=php;;
       --pydoc|--python|--py) CMD=pydoc;;
-      --stdin) SAVE_STDIN=true; [ $CMD = vimserver ] && CMD=tab;;
-      --tab) CMD=tab;;
+      --stdin) SAVE_STDIN=true; CMD=${CMD:-tab};;
+      --tab) CMD=tab WAIT=true;;
       *) new_args=("${new_args[@]}" "$arg");;
     esac
   done
@@ -81,4 +85,5 @@ if $SAVE_STDIN; then
 fi
 
 #$CMD "$@"
-$CMD "${new_args[@]}"
+#$WAIT && wait
+$CMD "${new_args[@]}" >/dev/null
