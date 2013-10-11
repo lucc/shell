414b99b04ece327e9b96a84a9f6f2f816fd5e055
Lucas Hoffmann <l-m-h@web.de>
2013-10-11 18:39:27 +0200

    secure.sh: refactor to use shellfunctions

--- secure.sh
+++ secure.sh
@@ -6,6 +6,7 @@ METHOD=--text
 ACTION=
 
 help () {
+  # display help on commandline
   local PROG="`basename "$0"`"
   echo "Usage: $PROG [-g] [ -m | -u ]"
   echo "       $PROG -h"
@@ -13,9 +14,50 @@ help () {
   echo "You can use the options to force (un)mounting or see this help."
 }
 
-if [ `uname` = Darwin ]; then
-  alias truecrypt=/Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt
-fi
+tc () {
+  # wrapper for truecrypt to pass standard options
+  local truecrypt=truecrypt
+  if [ `uname` = Darwin ]; then
+    truecrypt=/Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt
+  fi
+  $truecrypt $METHOD --verbose --keyfiles= "$@"
+}
+
+create_new_container () {
+  # create a new container, double the size of the old one
+  local dir=`mktemp -d -t $$.XXXXXX`
+  local size=`wc -c < $CONTAINER`
+  mkdir -p $dir
+  echo Creating new container ...
+  tc                            \
+    --create                    \
+    --encryption=AES            \
+    --hash=RIPEMD-160           \
+    --random-source=/dev/random \
+    --size=$((2 * size))        \
+    --volume-type=normal        \
+    ${CONTAINER}.new
+  echo Mounting new container to copy files ...
+  mount ${CONTAINER}.new $dir
+  cp -rv "$MOUNTPOINT/"* $dir
+  echo Unmounting and moving containers ...
+  dismount $dir
+  dismount $MOUNTPOINT
+  mv -v $CONTAINER ${CONTAINER}.old
+  mv -v ${CONTAINER}.new $CONTAINER
+  echo Mounting new container ...
+  mount $CONTAINER $MOUNTPOINT
+}
+
+mount_tc () {
+  # mount a truecrypt container
+  tc --protect-hidden=no "$@"
+}
+
+dismount () {
+  # unmount a truecrypt container from the given mountpoint
+  tc --dismount "$@"
+}
 
 if mount | grep -q $MOUNTPOINT; then
   ACTION=unmount
@@ -25,6 +67,7 @@ fi
 
 while getopts ghmu FLAG; do
   case $FLAG in
+    c) ACTION=create;;
     g) METHOD=;;
     h) ACTION=help;;
     m) ACTION=mount;;
@@ -40,13 +83,16 @@ if [ $# -ne 0 ]; then
 fi
 
 case $ACTION in
+  create)
+    create_new_container
+    ;;
   mount)
     echo Mounting ...
-    truecrypt $METHOD --protect-hidden=no --keyfiles= $CONTAINER $MOUNTPOINT
+    mount_tc $CONTAINER $MOUNTPOINT
     ;;
   unmount)
     echo Unmounting ...
-    truecrypt $METHOD --dismount $MOUNTPOINT
+    dismount $MOUNTPOINT
     ;;
   help)
     help
