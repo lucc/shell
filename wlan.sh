778400010ea16a17c3db48914beac53952b05569
Lucas Hoffmann <l-m-h@web.de>
2013-10-11 18:36:44 +0200

    airport wlan.sh: update scripts to handle airport

--- wlan.sh
+++ wlan.sh
@@ -8,3 +8,10 @@ networksetup -setairportpower en1 off
 sleep 1
 echo Starting airport ...
 networksetup -setairportpower en1 on
+
+# what is the difference? (from
+# http://alvinalexander.com/source-code/mac-os-x/how-restart-mac-os-x-networking-command-line
+# and
+# http://blog.jeffcosta.com/2011/02/06/restart-the-network-on-osx-from-command-line/)
+#sudo ifconfig en0 down
+#sudo ifconfig en0 up
