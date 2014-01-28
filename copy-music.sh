7e5774c6b9d931f57b883081b3857488d27ff1b9
Lucas Hoffmann <l-m-h@web.de>
2014-01-28 08:42:29 +0100

    copy-music.sh: new script
    New script to batch convert music to ogg format.

---
+++ copy-music.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+# a script to convert all music files in the source directory to a uniform
+# format, saving them in destination directory
+
+PROG="`basename "$0"`"
+if [ -t 1 -a -t 2 ]; then
+  COLOR="\033[1m"
+  NOCOLOR="\033[m"
+else
+  COLOR=
+  NOCOLOR=
+fi
+
+usage () {
+  echo "$PROG src target"
+  echo "Convert all music files in src to ogg files into target."
+}
+
+die () {
+  ret=$1
+  shift
+  echo "$PROG: $@" >&2
+  exit $ret
+}
+
+deref () {
+  file="$1"
+  while [ -l "$file" ]; do
+    file="`readlink "$file"`"
+  done
+  echo "$file"
+}
+
+convert_file () {
+  ffmpeg              \
+    -nostdin          \
+    -loglevel warning \
+    -i "$1"           \
+    -acodec libvorbis \
+    "$dest/${1%.*}.ogg"
+  # -loglevel quiet   \
+  # -loglevel panic   \
+  # -loglevel fatal   \
+  # -loglevel error   \
+  # -loglevel warning \
+  # -loglevel info    \
+  # -loglevel verbose \
+  # -loglevel debug   \
+}
+
+if [ $# -ne 2 ]; then
+  usage
+  exit 2
+fi
+
+src="$1"
+dest="$2"
+
+mkdir -p "$dest"
+cd "$dest" || die 3 Can not cd to "$dest".
+dest="`pwd -P`"
+cd - >/dev/null 2>&1
+
+# change to the source directory to be able to work with relative filenames
+cd "$src" || die 3 Can not cd to "$src".
+
+# first duplicate the directory structure of $src to $dest
+#find . -type d -exec mkdir -p "$dest"/{} \;
+
+find . -type f | while read file; do
+  echo
+  echo "converting $COLOR$file$NOCOLOR ..."
+  mkdir -p "$dest"/"`dirname "$file"`"
+  convert_file "$file"
+done
