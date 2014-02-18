856768ff6d8d1c22cd7f38c7e5641882906e927f
Lucas Hoffmann <l-m-h@web.de>
2014-02-18 14:33:23 +0100

    fixperm.sh: add more extended attributes and ACL

--- fixperm.sh
+++ fixperm.sh
@@ -6,12 +6,18 @@
 # if no arguments are given work on the current directory
 if [ $# -eq 0 ]; then set .; fi
 
+# remove acl entries  TODO how to delete all of them?
+chmod -R -a# 0 "$@"
+
 # set unix file permissions
 find "$@"                                \
   \( -type f -exec chmod 600 {} + \) -or \
   \( -type d -exec chmod 700 {} + \)
 
 # delete apples extendet file attributes
-for attribute in com.apple.quarantine com.apple.FinderInfo; do
+for attribute in com.apple.quarantine                           \
+                 com.apple.FinderInfo                           \
+		 com.apple.metadata:_kTimeMachineNewestSnapshot \
+		 com.apple.metadata:_kTimeMachineOldestSnapshot; do
   xattr -rd $attribute "$@"
 done
