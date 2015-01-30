721842994e6e5c9e39d6dddae9897d9408ee09ad
Lucas Hoffmann <l-m-h@web.de>
2015-01-30 09:21:34 +0100

    Add messages to graphic driver switching script.

--- linux/arch-switch-nvidia-driver.bash
+++ linux/arch-switch-nvidia-driver.bash
@@ -1,6 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# copied from https://wiki.archlinux.org/index.php/Nvidia#Order_of_install.2Fdeinstall_for_changing_drivers
+cat <<EOF
+This script was copied from
+https://wiki.archlinux.org/index.php/Nvidia#Order_of_install.2Fdeinstall_for_changing_drivers
+
+It will try to find the installed nvidia or nouveau drivers and replace them
+by the others.
+EOF
 
 BRANCH=340xx
 #BRANCH=304xx
@@ -11,9 +17,21 @@ NOUVEAU="xf86-video-nouveau mesa-libgl"
 _NOUVEAU="$(pacman -Qqs ^mesa-libgl$)"
 
 if [[ ! $_NVIDIA ]]; then
-    pacman -Rdds $NOUVEAU
-    pacman -S $NVIDIA #lib32-$NVIDIA-libgl #$NVIDIA-lts
+  cat <<EOF
+
+No nvidia driver found, I guess you are using nouveau.  Trying to switch:
+  nouveau -> nvidia ...
+EOF
+  pacman -Rdds $NOUVEAU
+  pacman -S $NVIDIA #lib32-$NVIDIA-libgl #$NVIDIA-lts
 elif [[ ! $_NOUVEAU ]]; then
-    pacman -Rdds $_NVIDIA
-    pacman -S $NOUVEAU #lib32-mesa-libgl
+  cat <<EOF
+
+No nouveau driver found, I guess you are using nvidia  Trying to switch:
+  nvidia -> nouveau ...
+EOF
+  pacman -Rdds $_NVIDIA
+  pacman -S $NOUVEAU #lib32-mesa-libgl
+else
+  echo Did not find any matching driver. What does that mean?
 fi
