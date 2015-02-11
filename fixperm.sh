745f10c42ba54a594c5678b8cdba0540e2c3857f
Lucas Hoffmann <l-m-h@web.de>
2015-02-11 10:32:35 +0100

    Refactor fixperm script for portability.

--- fixperm.sh
+++ fixperm.sh
@@ -1,26 +1,47 @@
 #!/bin/sh
-#
-# change the permissions and extendet file attributes for the given files and
+
+# Change the permissions and extendet file attributes for the given files and
 # directories or the current directory.  Always work recursivly.
+
+prog=`basename "$0"`
+usage () {
+  echo "Usage: $prog [path ...]"
+  echo "       $prog -h"
+}
+rm_acl () {
+  # remove acl entries  TODO how to delete all of them?
+  chmod -RN "$@"
+}
+chmod_R () {
+  # set unix file permissions
+  find "$@"                                \
+    \( -type f -exec chmod 600 {} + \) -or \
+    \( -type d -exec chmod 700 {} + \)
+  # alternatively
+  #chmod -R u+rwX,go-rwx "$@"
+}
+xattr_r () {
+  # delete apples extendet file attributes
+  local attributes=
+  attributes="$attributes com.apple.quarantine"
+  attributes="$attributes com.apple.FinderInfo"
+  attributes="$attributes com.apple.metadata:_kTimeMachineNewestSnapshot"
+  attributes="$attributes com.apple.metadata:_kTimeMachineOldestSnapshot"
+  for attribute in $attributes; do
+    xattr -rd $attribute "$@"
+  done
+  # alternatively
+  #xattr -rc "$@"
+}
+
+if [ "$1" = -h ]; then
+  usage
+  exit
+fi
 
 # if no arguments are given work on the current directory
 if [ $# -eq 0 ]; then set .; fi
 
-# remove acl entries  TODO how to delete all of them?
-chmod -RN "$@"
-
-# set unix file permissions
-find "$@"                                \
-  \( -type f -exec chmod 600 {} + \) -or \
-  \( -type d -exec chmod 700 {} + \)
-
-# delete apples extendet file attributes
-for attribute in com.apple.quarantine                           \
-                 com.apple.FinderInfo                           \
-		 com.apple.metadata:_kTimeMachineNewestSnapshot \
-		 com.apple.metadata:_kTimeMachineOldestSnapshot; do
-  xattr -rd $attribute "$@"
-done
-# alternatively
-#xattr -rc "$@"
-#chmod -R u+rwX,go-rwx "$@"
+if [ `uname` = Darwin ]; then rm_acl "$@"; fi
+chmod_R "$@"
+if [ `uname` = Darwin ]; then xattr_r "$@"; fi
