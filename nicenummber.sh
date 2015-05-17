0fbd78d3d865b11dcc205bf0eb468652a53abf99
Lucas Hoffmann <l-m-h@web.de>
2015-05-18 01:37:33 +0200

    numbernice: Move some option handling to perl.

--- nicenummber.sh
+++ nicenummber.sh
@@ -6,26 +6,18 @@ options=-n
 pattern=
 # name of the rename executable (sometimes it is perl-rename)
 rename=
-regex=false
 
 usage () {
   # print usage information for the user
   local prog="`basename "$0"`"
-  echo "$prog [-fnqrsvx] pattern [file ...]"
-  echo "$prog [-fnqrsvx] -p pattern [file ...]"
+  echo "$prog [-fnqvx] pattern [file ...]"
+  echo "$prog [-fnqvx] -p pattern [file ...]"
   echo "$prog -h"
   echo "The pattern must contain one #, where the numbers are."
   echo "Unless -f is given no renaming is done"
   echo "There must be a string before and after the hash symbol for this"
   echo "program to work reliably."
   echo TODO: explain more ...
-}
-die () {
-  # exit with an error message and an error code
-  local ret="$1"
-  shift
-  echo "$@" >&2
-  exit "$ret"
 }
 find_rename () {
   # find the correct rename executable
@@ -39,32 +31,34 @@ find_rename () {
   return 1
 }
 prepare_perl_expresion () {
-  # Print the perl expresion used for renameing.  $1 and $2 are the parts of
-  # the pattern given by the user, on stdin is the list of files.
+  # Print the perl expresion used for renameing.
   perl -we '
+    use strict;
     use List::Util qw(max);
-    my ($re1, $re2) = split("#", shift); # split the pattern at the "#" char
-    my $max = max(map { /.*?$re1(\d+)$re2.*/; length $1 } <>);
+    my $pattern = shift;
+    die("No pattern given.") if (! $pattern);
+    die("Pattern does not contain a # character.") if ! ($pattern =~ /#/);
+    my ($re1, $re2) = split("#", $pattern); # split the pattern at the "#" char
+    my $max = max(map { /.*?$re1(\d+)$re2.*/; length $1 } glob("*"));
+    my $expr = "";
     for (my $i = 1; $i < $max; $i++) {
-      print "s/$re1(\\d{$i})$re2/${re1}0\$1$re2/;";
-    }' -- "$@"
+      $expr .= "s/$re1(\\d{$i})$re2/${re1}0\$1$re2/;";
+    }
+    print $expr' -- "$@"
 }
 
 if ! find_rename; then
-  die 1 Can not find the correct rename executable.
+  echo Can not find the correct rename executable.
+  exit 1
 fi
 
 # parse the command line
-while getopts fhnp:qrsvx FLAG; do
+while getopts fhnp:x FLAG; do
   case $FLAG in
     f) options=;;
     h) usage; exit;;
     n) options=-n;;
     p) pattern="$OPTARG";;
-    q) quiet=true;;
-    r) regex=true;;
-    s) regex=false;;
-    v) quiet=false;;
     x) set -x;;
     *) usage; exit 2;;
   esac
@@ -76,12 +70,8 @@ if [ -z "$pattern" ]; then
   pattern="$1"
   shift
 fi
-if [ -z "$pattern" ]; then
-  die 2 No pattern given.
+if [ $# -eq 0 ]; then
+  set -- *
 fi
-if echo "$pattern" | grep -qv \#; then
-  die 2 Pattern does not contain a \# character.
-fi
-
-perl_expr=`ls | prepare_perl_expresion "$pattern"`
+perl_expr=`prepare_perl_expresion "$pattern"` || exit 1
 $rename $options "$perl_expr" "$@"
