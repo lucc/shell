3d820e0fe67262980a98a6b1174b70ed234967ba
Lucas Hoffmann <l-m-h@web.de>
2015-11-15 09:11:26 +0100

    Add small wrapper for amixer.

---
+++ linux/volume.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# A simple script to collect some volume setting actions into a nice
+# interface.
+
+prog="$(basename "$0")"
+device=Master
+done=false
+
+amixer_set () {
+  amixer --quiet set $device "$@"
+}
+
+mute () {
+  amixer_set mute
+}
+
+unmute () {
+  amixer_set unmute
+}
+
+set_volume () {
+  amixer_set ${1:-100}%
+}
+
+inc_volume () {
+  amixer_set ${1:-1}%+
+}
+
+dec_volume () {
+  amixer_set ${1:-1}%-
+}
+
+usage () {
+  echo "$prog [-mu] [[+-]num]"
+  echo "$prog -h"
+}
+
+while getopts hmud:0123456789 FLAG; do
+  case $FLAG in
+    h) help; exit;;
+    m) mute; done=true;;
+    u) unmute; done=true;;
+    d) device="$OPTARG";;
+    [0-9]*) true;; # skip numbers
+    *) usage; exit 2;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+func=
+if [ $# -eq 1 -o $# -eq 2 ]; then
+  case "$1" in
+    +) func=inc_volume; shift;;
+    -) func=dec_volume; shift;;
+    +[0-9]*) func=inc_volume; set "${1#+}";;
+    -[0-9]*) func=dec_volume; set "${1#-}";;
+    [0-9]*) func=set_volume;;
+    *) usage; exit 2;;
+  esac
+  case "$1" in
+    [0-9]|[0-9][0-9]|100) true;;
+    *) usage; exit 2;;
+  esac
+elif [ $# -eq 0 ]; then
+  func=set_volume
+else
+  usage
+  exit 2
+fi
+
+$func $1
