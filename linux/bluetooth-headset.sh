05c1d36ed932ec14ce95fc251bdbf9598067f7bc
Lucas Hoffmann <l-m-h@web.de>
2016-07-13 12:42:54 +0200

    bluetooth-headset: Add more options to script

--- linux/bluetooth-headset.sh
+++ linux/bluetooth-headset.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 
+# Connect to the bluetooth headset.
+
+version=1
+prog="${0##*/}"
+
+usage () {
+  echo "Usage: $prog [-cdpsx]"
+  echo "       $prog -h"
+  echo "       $prog -v"
+}
+
+help () {
+  echo "Connect to the bluetooth headset."
+  echo
+  echo "  -h    display help"
+  echo "  -v    display version"
+  echo "  -x    debugging output"
+  echo
+  echo "  -c    connect the headset"
+  echo "  -d    disconnect the headset"
+  echo "  -s    stop the bluetooth service"
+  echo "  -p    power off bluetooth"
+}
+
+bluetooth_service_running () {
+  systemctl status bluetooth | grep -q 'Status: "Running"'
+}
+
 connect_to_headset () {
+  check_bluttooth
   bluetoothctl <<EOF
 power on
 agent on
@@ -13,25 +42,37 @@ EOF
 }
 
 bluetooth_poweroff () {
-  bluetoothctl <<EOF
+  if bluetooth_service_running; then
+    bluetoothctl <<EOF
 power off
 exit
 EOF
+  fi
 }
 
 check_bluttooth () {
-  if ! systemctl status bluetooth | grep -q 'Status: "Running"'; then
+  if ! bluetooth_service_running; then
     echo Starting bluetooth ...
     sudo systemctl start bluetooth
   fi
 }
 
-while getopts h FLAG; do
+while getopts cdhpsvx FLAG; do
   case $FLAG in
-    h) echo connect to bluetooth headset >&2; exit;;
-    *) echo Error >&2; exit 2;;
+    c) connect_to_headset; exit;;
+    d) echo disconnect: TODO; exit 2;;
+    h) usage; echo; help; exit;;
+    p) bluetooth_poweroff; exit;;
+    s)
+      bluetooth_poweroff
+      check_bluttooth && sudo systemctl stop bluetooth
+      exit;;
+    v) echo "$prog -- version $version"; exit;;
+    x) set -x;;
+    *) usage >&2; exit 2;;
   esac
 done
+shift $((OPTIND - 1))
 
-check_bluttooth
+# If no option was given connect the headset.
 connect_to_headset
