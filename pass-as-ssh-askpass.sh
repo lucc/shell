d7522481e788b39c8dd9c3a4d94298f9e9d66886
Lucas Hoffmann <l-m-h@web.de>
2017-10-18 02:24:21 +0200

    ssh-askpass: search for password file by ssh id filename

--- pass-as-ssh-askpass.sh
+++ pass-as-ssh-askpass.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-exec pass show ssh/id
+
+file=$*
+file=${file%: }
+file=${file#Enter passphrase for }
+if [ ! -f "$file" ]; then
+  file=${file#Bad passphrase, try again for }
+fi
+id=${file##*/}
+id=${id%.id_rsa}
+
+pass show ssh/"$id" 2>/dev/null || pass show ssh/id
