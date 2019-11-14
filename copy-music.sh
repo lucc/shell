4bcc882096e38ae95199dbc4a25dbfb9cc664205
terminalforlife <terminalforlife@yahoo.com>
2019-11-14 01:03:51 +0000

    Add minor optimizations & 'best practice' changes
    Since I haven't used `zsh` in a very long time, I wasn't able to do
    much, but what I was able to address:
    
      * Protected many unprotected strings and variables found throughout.
    
      * The use of `basename` and `dirname` were replaced with built-in
        functionality. It's unlikely to be an issue, unless you have a
        protensity towards files whose names bizarrely contain forward
        slashes.
    
    It seems you're primarily a Python and/or Perl programmer, so I
    understand why you weren't in the habit of quoting variables, but shell
    is very different.

--- copy-music.sh
+++ copy-music.sh
@@ -15,7 +15,7 @@
 
 # variables {{{1
 # the name of this program
-PROG=$(basename $0)
+PROG=${0##*/}
 # variable used by traps to stop the script (can be true or false)
 CONTINUE=true
 # array of directories with music to be converted
@@ -40,37 +40,36 @@ fi
 # functions {{{1
 # fuctions for user interaction and general script execution {{{2
 usage () { # {{{3
-  echo $PROG src target
-  echo Convert all music files in src to ogg files into target.
+  echo "$PROG src target"
+  echo "Convert all music files in src to ogg files into target."
 }
 
 die () { # {{{3
-  ret=$1
   shift
-  echo $PROG: $@ >&2
-  exit $ret
+  echo "$PROG: $*" >&2
+  exit $1
 }
 
 handle_signal () { # {{{3
   if $CONTINUE; then
     TIMESTAMP=$(date +%s)
     CONTINUE=false
-    echo Let me just finish converting $file ... >&2
+    echo "Let me just finish converting $file ..." >&2
   elif [[ $(($TIMESTAMP + 10)) -lt $(date +%s) ]]; then
     echo 'OK, if you force me.  Engines STOP!' >&2
     exit 10
   else
-    echo Let me just finish converting $file ... >&2
+    echo "Let me just finish converting $file ..." >&2
   fi
 }
 
 # filesystem interaction {{{2
 resolve_symlinks () { # {{{3
-  local file=$1
-  while [[ -l $file ]]; do
-    file=$(readlink $file)
+  local file="$1"
+  while [[ -l "$file" ]]; do
+    file=$(readlink "$file")
   done
-  echo $file
+  echo "$file"
 }
 
 # conversion functions {{{2
@@ -93,27 +92,27 @@ ffmpeg_wrapper () { # {{{3
 to_ogg () { # {{{3
   # TODO quality
   ffmpeg_wrapper       \
-    -i $1              \
+    -i "$1"            \
     -codec:a libvorbis \
     -f ogg             \
-    ${2%.ogg}.ogg
+    "${2%.ogg}.ogg"
 }
 
 to_mp3 () { # {{{3
   # TODO quality
   ffmpeg_wrapper       \
-    -i $1              \
+    -i "$1"            \
     -f mp3             \
-    ${2%.mp3}.mp3
+    "${2%.mp3}.mp3"
 }
 
 copy () { # {{{3
-  cp -a $1 $2
+  cp -a "$1" "$2"
 }
 
 convert () { # {{{3
   # the main conversion function
-  local file=$1
+  local file="$1"
 }
 
 parse_options () { # {{{2
@@ -125,21 +124,21 @@ parse_options () { # {{{2
     --longoptions src:,source:,out:,output: \
     --longoptions quality:,format:,merge \
     --longoptions passthrough-extension:,exclude: \
-    --name $PROG \
+    --name "$PROG" \
     -- $@)
   local err=$?
   eval set -- $opts
   # parse command line: read the options
   while [[ $# -ne 0 ]]; do
-    case $1 in
-      --passthrough-extension) passthrough_extensions+=$2; shift;;
-      -e|--exclude) exclude+=$2; shift;;
-      -f|--format) FORMAT=$2; shift;;
+    case "$1" in
+      --passthrough-extension) passthrough_extensions+="$2"; shift;;
+      -e|--exclude) exclude+="$2"; shift;;
+      -f|--format) FORMAT="$2"; shift;;
       -h|--help) usage; exit;;
       -m|--merge) MERGE=true;;
-      -o|--out|--output) OUT=$2; shift;;
-      -q|--quality) QUALITY=$2; shift;;
-      -s|--src|--source) SRC+=$2; shift;;
+      -o|--out|--output) OUT="$2"; shift;;
+      -q|--quality) QUALITY="$2"; shift;;
+      -s|--src|--source) SRC+="$2"; shift;;
       -V|--version) version; exit;;
       -v|--verbose) verbose+=1;;
       --quiet) verbose=0;;
@@ -148,15 +147,15 @@ parse_options () { # {{{2
     esac
     shift
   done
-  if [[ $#SRC -eq 0 || -z $OUT ]]; then
-    die 2 You need to specify at least one input and output path.
+  if [[ $#SRC -eq 0 || -z "$OUT" ]]; then
+    die 2 "You need to specify at least one input and output path."
   fi
   typeset -a find_args
   find_args=(-type f)
   if [[ $#exclude -gt 0 ]]; then
     find_args+=\(
     for pattern in $exclude; do
-      find_args+=(-path $pattern -or)
+      find_args+=(-path "$pattern" -or)
     done
     find_args[-1]=( \) -prune -or -print)
   fi
@@ -165,20 +164,20 @@ parse_options () { # {{{2
 check_dirs () { # {{{2
   # check input and output directories for permissions
   # TODO: Does this work correctly?
-  if mkdir -p $OUT; then
-    ( cd $OUT || die 3 Can not cd to $OUT. )
-    dest=$(cd $OUT && pwd -P)
+  if mkdir -p "$OUT"; then
+    ( cd "$OUT" || die 3 "Can not cd to $OUT." )
+    dest=$(cd "$OUT" && pwd -P)
   else
-    die 3 Can not use $OUT as output directory.
+    die 3 "Can not use $OUT as output directory."
   fi
   for dir in $SRC; do
-    if [[ -d $dir ]] && [[ -r $dir ]]; then
+    if [[ -d "$dir" ]] && [[ -r "$dir" ]]; then
       continue
       # TODO This does not work with multible input dirs.
       # change to the source directory to be able to work with relative filenames
       #cd "$src" || die 3 Can not cd to "$src".
     else
-      die 2 $dir is not a readable directory.
+      die 2 "$dir is not a readable directory."
     fi
   done
 }
@@ -186,12 +185,12 @@ check_dirs () { # {{{2
 main () { # {{{2
   local srcdir currdest
   for srcdir in $SRC; do
-    if ! $MERGE; then
-      currdest=$dest/$(basename $srcdir)
+    if ! "$MERGE"; then
+      currdest="$dest/${srcdir##*/}"
     else
-      currdest=$dest
+      currdest="$dest"
     fi
-    find $srcdir $find_args | LC_ALL=C sort --ignore-case | \
+    find "$srcdir" $find_args | LC_ALL=C sort --ignore-case | \
 	while read -r file && $CONTINUE; do
       command=to_ogg
       for extension in passthrough_extensions; do
@@ -200,14 +199,14 @@ main () { # {{{2
 	  break
 	fi
       done
-      target=$dest/${file%.*}.$FORMAT
+      target="$dest/${file%.*}.$FORMAT"
       printf "\nconverting $COLOR$file$NOCOLOR ..."
       if [[ $target -nt $file ]]; then
 	echo " $target is newer.  Skipping."
       else
 	echo
-	mkdir -p $(dirname $target)
-	$command $file $target
+	mkdir -p "${target%/*}"
+	$command "$file" "$target"
       fi
     done
   done
