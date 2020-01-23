32e0aba0ed164e88bac8b69c591f665b550bfd4b
Lucas Hoffmann <lucc@posteo.de>
2020-01-23 09:51:31 +0100

    ssh: find the ssh key password by comment in the password store

--- pass-as-ssh-askpass.sh
+++ pass-as-ssh-askpass.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+set -e
+
+# First extract the file name from the command line arguments.
 file=$*
 file=${file%: }
 file=${file#Enter passphrase for }
 if [ ! -f "$file" ]; then
   file=${file#Bad passphrase, try again for }
 fi
+
+# Check that we did find the file name.
+if [ ! -f "$file" ]; then
+  echo "Error: can not find file name of the key file in '$*'." >&2
+  exit 1
+fi
+
+# Try the file name as the id in the password store.
 id=${file##*/}
 id=${id%.id_rsa}
-
-pass show ssh/"$id" 2>/dev/null || pass show ssh/id
+if ! pass show ssh/"$id" 2>/dev/null; then
+  # If that fails, extract the comment from the ssh key and try that as an id
+  # in the password store.
+  id=$(cut -f3- -d ' ' < "$file".pub)
+  pass show ssh/"$id"
+fi
