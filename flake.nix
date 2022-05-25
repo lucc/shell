8e73414f51a7ee26ac9076389fe739d69231e67c
Lucas Hoffmann <lucc@posteo.de>
2022-05-25 06:53:08 +0200

    flake: Restructure code and add term script to derivation

--- flake.nix
+++ flake.nix
@@ -1,30 +1,36 @@
 {
   description = "Some shell scripts";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
 
-    defaultPackage.x86_64-linux =
-      let
-        #inherit (nixpkgs.legacyPackages.x86_64-linux) resholve;
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in pkgs.stdenv.mkDerivation {
-        name = "luccs-scripts";
-        src = self;
-        installPhase = ''
-          install -D -t $out/bin \
-            diff-wrapper.bash \
-            file+ \
-            git/git-credential-pass \
-            git/git-is-dirty \
-            git/git-multi-rebase \
-            git/git-retry-rebase \
-            git/git-tag-stats \
-            git/git-wc-statistics \
-            linux/auto-xrandr \
-            linux/bluetooth-headset.sh \
-            tmux-youtube-dl.sh \
+  let
+    system = "x86_64-linux";
+    scripts = [
+      "diff-wrapper.bash"
+      "file+"
+      "git/git-credential-pass"
+      "git/git-is-dirty"
+      "git/git-multi-rebase"
+      "git/git-retry-rebase"
+      "git/git-tag-stats"
+      "git/git-wc-statistics"
+      "linux/auto-xrandr"
+      "linux/bluetooth-headset.sh"
+      "term"
+      "tmux-youtube-dl.sh"
+    ];
+    #inherit (nixpkgs.legacyPackages.x86_64-linux) resholve;
+    pkgs = import nixpkgs { inherit system; };
+  in
 
-          '';
+  {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "luccs-scripts";
+      src = self;
+      installPhase = ''
+        install -D -t $out/bin ${pkgs.lib.strings.concatStringsSep " " scripts}
+      '';
       };
+    defaultPackage.${system} = self.packages.${system}.default;
   };
 }
