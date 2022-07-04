f1bac6ea168112f997608dad8938fc6eae2840f9
Lucas Hoffmann <luc@artsoftandmore.com>
2022-07-04 18:05:48 +0200

    Add script to watch a url and send mail on changes

---
+++ watch-url.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+usage () {
+  echo "Usage: "${0##*/} [-i sec] [-o] url address [address ...]
+  echo "       "${0##*/} -t url
+}
+help () {
+  cat <<-EOF
+	Watch an URL and if it changes send a mail
+	EOF
+}
+fetch () {
+  curl --location --silent --connect-timeout 10 "$@"
+}
+
+once=false
+test=false
+interval=60
+while getopts hi:ot FLAG; do
+  case $FLAG in
+    h) usage; help; exit;;
+    i) interval=$OPTARG;;
+    o) once=true;;
+    t) test=true;;
+    *) usage >&2; exit 2;;
+  esac
+done
+
+shift $((OPTIND-1))
+url=$1
+shift
+addresses=("$@")
+
+dir=$(mktemp -d)
+trap 'rm -rf "$dir"' EXIT
+
+cd "$dir"
+fetch "$url" > new
+
+if "$test"; then
+  mv new old
+  sleep 1
+  fetch "$url" > new
+  diff old new
+  exit
+fi
+
+while sleep "$interval"; do
+  mv new old
+  fetch "$url" > new
+  if ! diff -q old new; then
+    date "+%F %T: change detected"
+    mail -s "URL Watcher: $url changed" "${addresses[@]}" <<-EOF
+	The URL $url has changed during the last $iterval seconds.
+	EOF
+    if "$once"; then exit; fi
+  fi
+done
