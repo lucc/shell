3514ab61214c30b64bbbb967203ee156063ff371
Lucas Hoffmann <lucc@posteo.de>
2024-12-27 10:26:14 +0100

    flake: improve build commands and shebang patching

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   description = "Some shell scripts";
 
-  outputs = { self, nixpkgs }:
-
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
     scripts = [
       "ticket.py"
@@ -21,31 +22,32 @@
       "linux/bluetooth-headset.sh"
       "term"
     ];
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {inherit system;};
     concat = pkgs.lib.strings.concatMapStrings (s: " ${self}/${s}");
-
-    #inherit (pkgs) resholve;
-    #zsh = [ "git/git-multi-rebase" ];
-    #scripts' = concat (pkgs.lib.lists.subtractLists zsh scripts);
+    build = name: buildInputs: command:
+      pkgs.runCommandLocal name {inherit buildInputs;} ''
+        ${command}
+        patchShebangs "$out/bin"
+      '';
+    # TODO check out resholve
 
     # it seems that youtube-dl in nixpkgs is not updated, use a replacement
-    tmux-youtube-dl = pkgs.runCommandLocal "tmux-youtube-dl" {} ''
+    tmux-youtube-dl = build "tmux-youtube-dl" [] ''
       install -D -t "$out/bin" ${self}/tmux-youtube-dl.sh
       substituteInPlace "$out/bin/tmux-youtube-dl.sh" \
-        --replace "command youtube-dl" ${pkgs.yt-dlp}/bin/yt-dlp
-      patchShebangs "$out/bin"
+        --replace-fail "command youtube-dl" ${pkgs.lib.meta.getExe pkgs.yt-dlp}
     '';
 
-    simple-scripts = pkgs.runCommandLocal "simple-scripts" {} ''
+    simple-scripts = build "simple-scripts" (with pkgs; [zsh python3]) ''
       install -D -t "$out/bin" ${concat scripts}
-      patchShebangs "$out/bin"
     '';
-    in
-
-    {
-      packages.${system}.default = pkgs.buildEnv {
+  in {
+    packages.${system} = {
+      default = pkgs.buildEnv {
         name = "luccs-scripts";
-        paths = [ simple-scripts tmux-youtube-dl ];
+        paths = [simple-scripts tmux-youtube-dl];
       };
+      inherit simple-scripts tmux-youtube-dl;
     };
+  };
 }
