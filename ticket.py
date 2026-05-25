d27c8b58e58b63ab0cf2a4bb0ac6b71cd139acdd
Lucas Hoffmann <lucc@posteo.de>
2026-05-25 23:14:16 +0200

    ticket: switch from adb-sync to better-adb-sync
    https://github.com/NixOS/nixpkgs/issues/523023
    https://github.com/NixOS/nixpkgs/pull/523025

--- ticket.py
+++ ticket.py
@@ -283,7 +283,7 @@ def sync_to_phone(folder: Path, todo: list[tuple[Path, date | None]]) -> None:
     with tempfile.TemporaryDirectory(dir=folder) as src:
         for file, _date in todo:
             os.link(file, os.path.join(src, file.name))
-        run(["adb-sync", "--delete", src+"/", target], check=True)
+        run(["adbsync", "--del", "push", src+"/", target], check=True)
 
 
 def handle_tickets() -> None:
